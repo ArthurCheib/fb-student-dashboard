89f2705b40d8c5cc7ad51662e9d04aa4fd89c465
Arthur Cheib <arthursilvacheib@gmail.com>
2024-03-09 16:13:30 -0300

    adding exercises
    in-class coding exercises

---
+++ fb-dashboard/in-class/in-class-coding_1.py
@@ -0,0 +1,25 @@
+#### In-class Coding 1 ####
+
+## Exercício 1 - Desenvolva seu primeiro programa! ##
+
+# Especificações:
+#   - Printar dois 'positional arguments'
+
+#   - Use um *keyword argument* da função print para
+#     ter uma exclamação no final da mensagem
+
+#   - Use um *keyword argument* da função print para
+#     ter um separador entre os *positional arguments*
+
+## Indentação (indentation)
+
+
+## (b) Mudando nosso programa
+#   - Crie um programa que dê olá ("Hello") para você (seu nome)!
+
+
+## (c) Melhorando nosso programa I
+#   - Faça com que seu programa te pergunte qual o seu nome,
+#     antes de dar Olá
+
+
---
+++ fb-dashboard/in-class/in-class-coding_4.py
@@ -0,0 +1,57 @@
+#### In-class Coding 4 ####
+
+## Sobre o Programa a ser criado:
+# Programa de cadastro e gestão de livros da
+# biblioteca da Faculdade Belavista
+    
+
+## PARTE 1 ##
+
+# INPUTS:
+
+# (a) Faça um programa que obtenha três informações
+#     sobre um livro a ser registrado na biblioteca: 
+    
+#       (i) nome completo do livro   
+#       (ii) autor do livro
+#       (iii) gênero do livro
+
+            
+
+# OUTPUTS:
+
+# (b) Continue codando seu programa - faça com que ele
+#     retorne uma mensagem contendo as seguintes infos do livro:
+    
+#      (i) Nome do livro (deve ser 'title-cased')
+
+#      (ii) Nome do autor do livro (todo em caixa alta)
+
+#      (iii) O genêro do livro. No entanto, substitua o gênero
+#            do livro que foi inserido por um gênero padrão         
+
+#      (iv) Um código numérico para o livro
+
+
+## PARTE 2 ##
+
+# TESTANDO O PROGRAMA
+
+
+# Escolha três livros e rode seu programa para eles
+
+# Livro 1
+
+# Livro 2
+
+# Livro 3
+
+
+
+## PARTE 3 ##
+
+# TRANFORMANDO O PROGRAMA EM FUNÇÃO
+
+
+
+
