9c3ba8e94f22529c5967ccb4f91261568eb9a1b5
Arthur Cheib <arthursilvacheib@gmail.com>
2024-03-11 22:55:35 -0300

    adding in-class 5

---
+++ fb-dashboard/in-class/in-class-coding_5.py
@@ -0,0 +1,30 @@
+#### In-class Coding 5 ####
+
+## Sobre o Programa a ser criado:
+# Programa de cadastro e gestão dos usuários e livros da
+# biblioteca da Faculdade Belavista
+
+## PARTE 1 - Validação de Dados do Usuário ##
+
+# (a) Adicione uma validação para o e-mail do usuário. 
+#     O e-mail deve terminar com "@faculdadebelavista.edu.br". 
+#     Se não terminar, peça ao usuário para inserir um e-mail válido.
+
+
+
+
+## PARTE 2 - Confirmação de Cadastro ##
+
+# (b) Após cadastrar um livro ou usuário, pergunte ao usuário
+#     se ele deseja realizar outro cadastro.
+#     Se sim, repita o processo; se não, encerre o programa.
+
+
+
+
+## PARTE 3 - Atualização da Validação de Gênero ##
+
+# (c) Modifique a validação do gênero do livro para garantir
+#     que ele esteja dentro de uma lista pré-definida de gêneros
+#     aceitáveis. Se não estiver, atribua um gênero padrão 'Geral'
+
