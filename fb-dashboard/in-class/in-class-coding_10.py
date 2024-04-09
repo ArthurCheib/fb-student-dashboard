2da4f5f8f73772b959ba105799cd85a649d7891c
Arthur Cheib <arthursilvacheib@gmail.com>
2024-04-09 09:35:50 -0300

    new slides

---
+++ fb-dashboard/in-class/in-class-coding_10.py
@@ -0,0 +1,25 @@
+#### In-class Coding 10 ####
+
+### Sobre a atividade:
+    
+## Nesta atividade, vocês vão criar um programa Python para ler
+# dados de renda, classificá-los em diferentes categorias e =
+# salvar essas informações em um novo arquivo.
+
+
+## (1) Abram o arquivo `income.csv` usando with open().
+
+## (2) Utilizando list comprehension: converta os valores
+#      lidos em (1) de string para números (int)
+
+## (3) Crie uma lista que contenha uma classificação de cada
+#      renda familiar média da lista do exercício (2), como:
+    
+    # < 2500 = 'Classe E'
+    # >= 2500 and < 5000 = 'Classe D'
+    # >= 5000 and < 7500 = 'Classe C'
+    # >= 7500 and < 10000 = 'Classe B'
+    # >= 10000 = 'Classe A'
+    
+## (4) Crie um novo arquivo csv, que contenha agora as duas
+#      informações: o valor da renda, classificação da renda
