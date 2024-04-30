a4846e39be2457d8dd2824e6efe0246aa52dece6
Arthur Cheib <arthursilvacheib@gmail.com>
2024-04-30 17:03:22 -0300

    Adding labs
    Adding lab#1 and #2 (final version of both)

---
+++ fb-dashboard/labs/lab1-dados-previos.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# Dados pré-definidos do jogop
+capital_inicial = 10000
+caixa = 0
+investido = 0
+custo_refeicao = 20
+preco_refeicao = 25
+
+# Rodadas do jogo
+meses = ['Janeiro', 'Fevereiro', 'Março',
+         'Abril', 'Maio', 'Junho']
+
+# Eventos inesperados
+eventos = ['Mesada extra para os alunos: +15%',
+           'Crise Americana: -40%',
+           'Primeiro estágio: +25%',
+           'Guerra na Argentina:  -35%',
+           'Passe livre em SP: +20%',
+           'Aumento do preço do petróleo: -40%',
+           'Primeiro bônus: +35%',
+           'Redução nacional das exportações: -45%',
+           'Queda da bolsa: -50%']
