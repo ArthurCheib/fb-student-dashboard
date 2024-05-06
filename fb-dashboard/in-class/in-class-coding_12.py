096bf0d62163a594e00f11b71682f49cff2eb331
Arthur Cheib <arthursilvacheib@gmail.com>
2024-05-06 12:55:43 -0300

    new-in-class
    New exercise

---
+++ fb-dashboard/in-class/in-class-coding_12.py
@@ -0,0 +1,43 @@
+#### In-class Coding 12 ####
+
+### Sobre a atividade:
+    
+## Esse in-class coding treinará sua habilidade de manipular
+# e transformar dados utilizando o pacote Pandas
+
+import pandas as pd
+
+## Questão 1
+# Importe o dataset de crimes de Chicago de 2022
+# Exiba as primeiras e as últimas cinco linhas.
+# Quais são os tipos de dados de cada coluna?
+
+
+
+## Questão 2
+# Selecione todos os registros de crimes que resultaram em uma prisão
+# e que foram classificados como 'THEFT'.
+
+
+
+## Questão 3
+#  Organize os dados pelo Date de maneira decrescente e utilize iloc para
+# selecionar os 10 primeiros registros.
+# O que esses registros indicam sobre a natureza dos crimes?
+
+
+
+## Questão 4
+# Qual é o tipo de crime mais comum em cada Distrito (District)? 
+
+
+
+## Questão 5
+# Crie uma nova coluna chamada 'Is Violent' que é True para crimes classificados
+# como 'ASSAULT', 'BATTERY', 'ROBBERY', 'HOMICIDE', e False para outros tipos.
+# Quantos crimes dessa natureza foram cometidos por distrito de Chicago?
+
+
+
+
+
