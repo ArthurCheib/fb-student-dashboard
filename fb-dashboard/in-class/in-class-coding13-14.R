354104eb5d8883919255470bacad9b435ebf91e5
Arthur Cheib <arthursilvacheib@gmail.com>
2024-05-14 07:34:25 -0300

    class20-21

---
+++ fb-dashboard/in-class/in-class-coding13-14.R
@@ -0,0 +1,25 @@
+## Qual é a quantidade total de armas apreendidas (soma de todas as colunas de quantidade)
+# por ano e mês?
+
+
+
+## Qual é a média mensal de homicídios dolosos por cada Região Integrada de Segurança Pública (id_risp)
+## ao longo do período disponível?
+
+
+
+## Qual é a proporção de diferentes tipos de armas apreendidas em cada Área Integrada de Segurança Pública (id_aisp)
+# no último ano disponível?
+
+
+
+## Crie um gráfico de linhas que mostre a tendência de apreensões de armas
+# de fabricação caseira ao longo dos anos.
+
+
+
+## Crie um gráfico de linhas que mostre as variáveis de policiais mortos em serviço,
+# independentemente da corporação, e a apreensão de armas.
+
+
+
